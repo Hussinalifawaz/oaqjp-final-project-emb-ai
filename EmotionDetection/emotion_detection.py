c11384b06573e8abf3a15d84e9d37ee757224dbc
Hussinalifawaz <husseinaliaskar123@gmail.com>
2026-03-12 14:37:58 -0400

    complated the task from 1 to 8

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -3,15 +3,23 @@ import json
 
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } }
     
-    response = requests.post(url, json=myobj, headers=headers)
+    response = requests.post(url, json=myobj, headers=header)
+    
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
     
     formatted_response = json.loads(response.text)
     emotions = formatted_response['emotionPredictions'][0]['emotion']
-    
-
     dominant_emotion = max(emotions, key=emotions.get)
     
     return {
@@ -22,5 +30,3 @@ def emotion_detector(text_to_analyse):
         'sadness': emotions['sadness'],
         'dominant_emotion': dominant_emotion
     }
-
-    
